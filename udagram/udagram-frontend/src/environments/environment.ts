1c03a90d85d65550a29b7d2f2f5c284af5c10ac2
narouz <mina.sarkies1995@gmail.com>
2023-01-11 12:29:33 +0400

    environment file changed

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'udagram-api',
-  apiHost:'http://udagram-api-dev.eba-ttpbhnb3.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'http://udagram-api-dev.eba-ttpbhnb3.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
