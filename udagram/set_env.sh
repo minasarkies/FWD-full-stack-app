1951bc75edf64d5a656ee0d312613a201c822dcc
narouz <mina.sarkies1995@gmail.com>
2023-01-13 21:20:34 +0400

    set_env added

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,13 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=Mru63HgtDLEacbg
-export POSTGRES_HOST=postgres.cqbgnj1jg55q.us-east-1.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mina-udagram
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
-export JWT_SECRET=hello-from-other-side
-export URL=udagram-api-dev.eba-ttpbhnb3.us-east-1.elasticbeanstalk.com
-export AWS_ACCESS_KEY_ID= "AKIATH2HWVDENYHQIO2O"
-export AWS_SECRET_ACCESS_KEY="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"
+export POSTGRES_USERNAME='postgres'
+export POSTGRES_PASSWORD='Mru63HgtDLEacbg'
+export POSTGRES_HOST='postgres.cqbgnj1jg55q.us-east-1.rds.amazonaws.com'
+export POSTGRES_DB='postgres'
+export AWS_BUCKET='arn:aws:s3:::mina-udagram'
+export AWS_REGION='us-east-1'
+export AWS_PROFILE='default'
+export JWT_SECRET='hello-from-other-side'
+export URL='udagram-api-dev.eba-ttpbhnb3.us-east-1.elasticbeanstalk.com'
