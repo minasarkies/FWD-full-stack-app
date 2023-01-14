c5bb85b020ae8b8ee72f42ba340719b05d5d473d
narouz <mina.sarkies1995@gmail.com>
2023-01-14 20:14:40 +0400

    set_env added

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME='postgres'
+export POSTGRES_PASSWORD='Mru63HgtDLEacbg'
+export POSTGRES_HOST='postgres.cqbgnj1jg55q.us-east-1.rds.amazonaws.com'
+export POSTGRES_DB='postgres'
+export AWS_BUCKET='arn:aws:s3:::mina-udagram'
+export AWS_REGION='us-east-1'
+export AWS_PROFILE='default'
+export JWT_SECRET='hello-from-other-side'
+export URL='udagram-api-dev.eba-ttpbhnb3.us-east-1.elasticbeanstalk.com'
