29aa394ed7c4fd236003b5a0f887d08bb150c18e
narouz <mina.sarkies1995@gmail.com>
2023-01-15 16:14:34 +0400

    build script changed

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'udagram-api',
-  apiHost: 'http://udagram-api-dev.eba-ttpbhnb3.us-east-1.elasticbeanstalk.com/api/v0',
+  apiHost: 'udagram-api-dev.eba-uyxqbpma.us-east-1.elasticbeanstalk.com/api/v0',
 };
 
 /*
